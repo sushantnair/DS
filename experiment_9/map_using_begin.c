d94f852c54323e3985730ab4de29832bf5af7326
Sushant Nair <sushantnair.developer@gmail.com>
2023-12-28 23:50:29 +0530

    Update experiment_9
    added code

---
+++ experiment_9/map_using_begin.c
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main()
+{
+    //declaration of map container
+    map<char, int> mymap;
+    mymap['a'] = 1;
+    mymap['b'] = 2;
+    mymap['c'] = 3;
+    //using begin() to print map
+    for(auto it = mymap.begin(); it != mymap.end(); it++)
+    {
+        cout<<it->first<<" = "<<it->second<<"\n";
+    }
+    return 0;
+}
